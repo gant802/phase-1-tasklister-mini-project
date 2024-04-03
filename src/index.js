25ddfa52f72ef9c2ccde1a0a17fd58dd2e9a5bca
Grant Cummings <grantcummings4429@gmail.com>
2024-04-03 19:50:32 -0400

    finished mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+let form = document.getElementById('create-task-form');
+let userInput = form.querySelector('#new-task-description');
+let tasksUl = document.getElementById('tasks');
+let dropdown = form.querySelector('#dropdown');
+// let highPriority = form.querySelector('#high');
+// let mediumPriority = form.querySelector('#medium');
+// let lowPriority = form.querySelector('#low');
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  addTask()
+  userInput.value = ""
+})
+
+
+function addTask() {
+  if (userInput.value !== "") {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = "X"
+  li.textContent = userInput.value + "   "
+  tasksUl.appendChild(li)
+  li.appendChild(btn)
+  btn.addEventListener('click', deleteTask)
+  
+  if (dropdown.value === 'high') {
+    li.style.backgroundColor = "red"
+  } else if (dropdown.value === 'medium') {
+    li.style.backgroundColor = "yellow"
+  } else if (dropdown.value === 'low') {
+    li.style.backgroundColor = "green"
+  }
+}
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
